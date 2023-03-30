3e8951c52614d466b645864925a1d8d9cf3fb82c
robnsiov <robnsiov@gmail.com>
2023-03-30 17:31:26 +0430

    add On-Demand Revalidation

--- pages/api/revalidate.tsx
+++ pages/api/revalidate.tsx
@@ -4,12 +4,13 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
+  if (req.query.secret !== process.env.SECRET) {
     return res.status(401).json({ message: "Invalid token" });
   }
-
   try {
-    await res.revalidate(req.query.path as string);
+    await res.revalidate("/");
+    await res.revalidate("/blog");
+    await res.revalidate(`/blog/${req.query.slug}`);
     return res.json({ revalidated: true });
   } catch (err) {
     return res.status(500).send("Error revalidating");
