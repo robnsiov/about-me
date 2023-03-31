867192d8c94e8c9ffdd26dccc41d54cb06040b14
robnsiov <robnsiov@gmail.com>
2023-03-31 20:21:11 +0430

    fix revalidate api

--- pages/api/revalidate.tsx
+++ pages/api/revalidate.tsx
@@ -4,15 +4,20 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.query.secret !== process.env.SECRET) {
+  if (req.body.secret !== process.env.SECRET) {
     return res.status(401).json({ message: "Invalid token" });
   }
-  try {
-    await res.revalidate("/");
-    await res.revalidate("/blog");
-    await res.revalidate(`/blog/${req.query.slug}`);
-    return res.json({ revalidated: true });
-  } catch (err) {
-    return res.status(500).send("Error revalidating");
-  }
+
+  const home = res.revalidate("/");
+  const blog = res.revalidate("/blog");
+  const bySlug = res.revalidate(`/blog/${req.body.slug}`);
+  const result = Promise.allSettled([home, blog, bySlug]);
+  result
+    .then(() => {
+      return res.json({ revalidated: true });
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).send("Error revalidating");
+    });
 }
