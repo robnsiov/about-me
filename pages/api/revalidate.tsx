46d74c06f249a9f5bba7e9f840c7b4521a4d96b0
robnsiov <robnsiov@gmail.com>
2023-03-31 23:40:56 +0430

    fix revalidate api

--- pages/api/revalidate.tsx
+++ pages/api/revalidate.tsx
@@ -30,8 +30,6 @@ export default async function handler(
 ) {
   try {
     await res.revalidate("/");
-    // await res.revalidate("/blog");
-    // await res.revalidate(`/blog/${req.body.slug}`);
     return res.json({ revalidated: true });
   } catch (err) {
     return res.status(500).send("Error revalidating");
