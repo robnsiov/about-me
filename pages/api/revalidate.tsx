1e1e6c279235851c066c501cb7b8d3c7ecd17f7e
robnsiov <robnsiov@gmail.com>
2023-04-06 17:45:09 +0430

    add unstable_onlyGenerated

--- pages/api/revalidate.tsx
+++ pages/api/revalidate.tsx
@@ -6,7 +6,7 @@ export default async function handler(
 ) {
   const home = res.revalidate("/", {unstable_onlyGenerated: true});
   const blog = res.revalidate("/blog" , {unstable_onlyGenerated: true});
-  const bySlug = res.revalidate(`/blog/${req.body.slug}`. );
+  const bySlug = res.revalidate(`/blog/${req.body.slug}`, {unstable_onlyGenerated: true});
   try {
     await Promise.allSettled([home, blog, bySlug]);
     return res.json({ revalidated: true });
